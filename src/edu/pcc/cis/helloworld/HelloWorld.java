f021d999476fcc2e786e1788eb2afb43eff250d4
CSblings <caleb.billings@pcc.edu>
2023-02-07 21:47:26 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,7 +1,9 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hellom, World!");
+        System.out.println(new Date());
     }
 }
