74bd2d105b4a563ebf59780e75817d2936a0e4cd
CSblings <caleb.billings@pcc.edu>
2023-02-07 21:51:29 -0800

    added author info

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,6 +1,8 @@
 package edu.pcc.cis.helloworld;
 import java.util.Date;
 
+//Author: Caleb Billings
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hellom, World!");
